c8244e46f403a34c81ce4b39b6c878bf265a6c53
TinaGaohub <440020959@gapps.yrdsb.ca>
2026-05-04 17:09:41 -0400

    part 2

--- Wombat.java
+++ Wombat.java
@@ -8,18 +8,66 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Wombat extends Actor
 {
+    GreenfootSound wombatSound = new GreenfootSound("Wombat.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[5];
+    GreenfootImage[] idleLeft = new GreenfootImage[5];
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
+    /**
+     * Constructor
+     */
+    public Wombat()
+    {
+        for(int i = 0; i < idleRight.length; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/Wombat/Wombat" + i + ".png");
+            idleRight[i].scale(100,100);
+        }
+        for(int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/Wombat/Wombat" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100,100);
+        }
+        
+        animationTimer.mark();
+        
+        setImage(idleRight[0]);
+    }
+    int imageIndex = 0;
+    public void animateWombat()
+    {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }else{
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+        
+    }
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-1);
+            move(-2);
+            facing = "left";
         }
         else if(Greenfoot.isKeyDown("right"))
         {
-            move(1);
+            move(2);
+            facing = "right";
         }
         
         eat();
+        animateWombat();
     }
     
     /**
@@ -32,6 +80,7 @@ public class Wombat extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createBanana();
             world.increaseScore();
+            wombatSound.play();
         }
     }
 }
